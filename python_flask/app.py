147657f5282beb5b71c26a8b291539ab5231ccaf
Dannyle1237 <danny.le3@mavs.uta.edu>
2023-04-11 14:26:58 -0500

    search algo Danny

--- python_flask/app.py
+++ python_flask/app.py
@@ -9,15 +9,21 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route("/home")
 def home():
     #Store column Disease in DB to variable
-    diseases = dbf.storeQueryToVar('''SELECT * FROM Disease''')
+    symptoms = dbf.storeQueryToVar('''SELECT * FROM Symptom''')
+    combinations = dbf.storeQueryToVar('''SELECT * FROM Combination''')
+    arr = ["dischromic_patches", "itching"]
+    if arr[0] in combinations[0][1]: #if "dischromic_patches" in dischromic_patches itching nodal_skin_eruptions skin_rash
+        if arr[1] in combinations[0][1]:
+            print("Is found")
+    print(combinations[0])
     #Because everything in a database is stored as a tuple(array), you can 
     #grab the first value of each item
-    for idx, item in enumerate(diseases):
-        diseases[idx] = item[0]
-    print(diseases)
+    for idx, item in enumerate(symptoms):
+        symptoms[idx] = item[0]
+    #print(symptoms)
 
 
-    return render_template("home.html", diseaseList = diseases)
+    return render_template("home.html", symptomList = symptoms)
 
 #Below runs webapp
 if __name__=='__main__':
