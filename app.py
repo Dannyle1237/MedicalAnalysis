6c3ad92e8dfb8f2a4edf828b9afe8bbe80c55cae
Dannyle1237 <danny.le3@mavs.uta.edu>
2023-04-17 05:52:36 -0500

    Jesus christ

--- app.py
+++ app.py
@@ -1,51 +1,111 @@
-from flask import Flask, request, render_template, request
+from flask import Flask, request, render_template, request, jsonify
+import json
 #this is the file to handle backend stuff of the webpage
 from python_database import database_func as dbf
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-symptoms = []
+def formatToRead(words):
+    words = words.replace('_', ' ')
+    words = words.title()
+    return str(words)
+
+def algorithm(arr):
+    #Only run algorithm if array is not empty
+    if(len(user_arr) != 0):
+        #reset diseases array
+        confirmed_diseases = []
+        #pulls row
+        for i in range (len(combinations)):     
+            count = 0
+            #pulls symptom list
+            for symptom in user_arr:            
+                #checks for symptoms in combinations[rows][column]     
+                if symptom in combinations[i][1]: 
+                    count += 1
+            if count == (len(user_arr)):
+                #print(combinations[i])
+                if combinations[i][0] not in confirmed_diseases:
+                    confirmed_diseases.append(combinations[i][0])       
+        #print("Confirmed diseases " + str(confirmed_diseases))
+    else:
+        confirmed_diseases = []
+    return confirmed_diseases
+
+#Global variables
+#All available symptoms and diseases in database
+symptoms = dbf.storeQueryToVar('''SELECT * FROM Symptom''')
+diseases = dbf.storeQueryToVar('''SELECT * FROM Disease''')
+#Because everything in a database is stored as a tuple(array), you can 
+#grab the first value of each item
+for idx, item in enumerate(symptoms):
+    symptoms[idx] = item[0]
+#clean the symptoms list for dropdown menu so its better for user to read
+dropdown_symptoms = []
+for symptom in symptoms:
+    cleaned_s = formatToRead(symptom)
+    dropdown_symptoms.append(cleaned_s)
+#print(dropdown_symptoms)
+
+#Combinations of all possible diseases and symptoms (Disease, list of symptoms separated by space as one string)
+combinations = dbf.storeQueryToVar('''SELECT * FROM Combination''')
+#Array to store diseases received from algorithm
 confirmed_diseases= []
+#Array of user inputted symptoms
 user_arr = []
+#Readable user arr
+read_arr = []
+
+
 
 @app.route("/")
-@app.route("/home")
 def home():
-    global symptoms, confirmed_diseases
-    #Store column Disease in DB to variable
-    symptoms = dbf.storeQueryToVar('''SELECT * FROM Symptom''')
-    #Because everything in a database is stored as a tuple(array), you can 
-    #grab the first value of each item
-    for idx, item in enumerate(symptoms):
-        symptoms[idx] = item[0]
-    #print(symptoms)
+    global symptoms, confirmed_diseases, user_arr, combinations, dropdown_symptoms, read_arr
+    #When page reloads reset the array and confirmed diseases
+    confirmed_diseases = []
+    user_arr = []
+    read_arr = []
 
-    print("Symptoms:" + str(symptoms))
-    return render_template("home.html", symptomList = symptoms)
+    return render_template("home.html", symptomList = dropdown_symptoms, confirmedDiseases = confirmed_diseases, userList = read_arr)
 
 @app.route('/', methods=['POST'])
+@app.route('/list', methods=['POST'])
 def my_form_post():
-    global symptoms, confirmed_diseases, user_arr
+    global symptoms, confirmed_diseases, user_arr, combinations, dropdown_symptoms, read_arr
 
-    text = request.form['symptom_input']
-    
-    user_arr.append(text)
-    
-    combinations = dbf.storeQueryToVar('''SELECT * FROM Combination''')
-    for i in range (len(combinations)):     #pulls row
-        count = 0
-        for symptom in user_arr:                 #pulls symptom list
-            if symptom in combinations[i][1]: #checks for symptoms in combinations[rows][column]\
-                count += 1
-        if count == (len(user_arr)):
-            print(combinations[i])
-            if combinations[i][0] not in confirmed_diseases:
-                confirmed_diseases.append(combinations[i][0]) 
-                    
-    #print("Confirmed diseases " + str(confirmed_diseases))
+    user_input = request.form['symptom_input']
+    #Clean user input to match data format
+    user_input = user_input.replace(' ', '_')
+    user_input = user_input.lower()
 
-    return render_template("home.html", symptomList = symptoms, confirmedDiseases = confirmed_diseases, userList = user_arr)
+    #Check for valid input
+    if(user_input not in user_arr and user_input in symptoms):
+        read_arr.append(formatToRead(user_input))
+        user_arr.append(user_input)
+        print(f"Symptom({user_input}) user inputted into array")
+    else:
+        print(f"Symptom({user_input}) not inputted cause it is invalid")
+    
+    confirmed_diseases = algorithm(user_arr)
+
+    return render_template("home.html", symptomList = dropdown_symptoms, confirmedDiseases = confirmed_diseases, userList = read_arr)
+
+@app.route('/delete-symptom', methods=['POST'])
+def delete_note():
+    global user_arr, read_arr, confirmed_diseases
+    id = json.loads(request.data)
+    read_arr.pop(int(id['idx']))
+    user_arr.pop(int(id['idx']))
+    confirmed_diseases = algorithm(user_arr)
+
+    return jsonify({})
+
+@app.route('/list')
+def list():
+    global symptoms, confirmed_diseases, user_arr, combinations, dropdown_symptoms, read_arr
+    return render_template("home.html", symptomList = dropdown_symptoms, confirmedDiseases = confirmed_diseases, userList = read_arr)
+
 
 @app.route("/about")
 def about():
--- static/main.js
+++ static/main.js
@@ -1,31 +1,83 @@
+//Code to add functionality to dropdown bar (When user clicks it is added)
+let current = "";
+let isMouseHover = false;
+
+dropdown_items = document.getElementsByClassName("dropdown_item")
+
+for(let i = 0; i < dropdown_items.length; i++){
+  //Mouseover effect for dropdown items to show it in search bar
+  dropdown_items[i].addEventListener("mouseover", function(e){
+    tgt = e.target;
+    if (tgt.classList.contains("dropdown_item")) {
+      e.preventDefault(); // cancel link
+      document.getElementById("symptom_input").value = tgt.innerHTML;
+    }
+  })
+  //Click effect to save value if user wants it
+  dropdown_items[i].addEventListener("mousedown", function(e){
+    console.log("Clicking");
+    tgt = e.target;
+    if (tgt.classList.contains("dropdown_item")) {
+      current = tgt.innerHTML;
+      document.getElementById("symptom_input").value = tgt.innerHTML;
+    }
+  })
+}
+
+closeButtons = document.getElementsByClassName("close")
+for(let i = 0; i < closeButtons.length; i++){
+  closeButtons[i].addEventListener("click", function(e){
+    idx = e.srcElement.offsetParent.id;
+    fetch('/delete-symptom', {
+      method: 'POST',
+      body: JSON.stringify({ update:"deleteSymptom", idx: idx}),
+    }).then((_res) => {
+        window.location.href ="/list";
+    });
+  })
+}
+
 //Stuff for search bar 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropdown() {
-    document.getElementById("dropdown_search").classList.toggle("show");
+  dd_search = document.getElementById("dropdown_search")
+  if(dd_search.classList.toggle("show")){
+    isMouseHover = true;
   }
+  else{
+    console.log("value of current:" + current)
+    document.getElementById("symptom_input").value = current;
+    isMouseHover = false;
+  }
+}
   
-  function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("symptom_input");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("dropdown_search");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      txtValue = a[i].textContent || a[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
+function filterFunction() {
+  //First save the value User has currently
+  current = document.getElementById("symptom_input").value;
+  console.log("Saving as current:" + current);
+
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("symptom_input");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("dropdown_search");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
     }
   }
+}
 
-function dostuff(){
-  var ul = document.querySelector('ul');
-  var li = document.createElement('li');
-  var text = document.createTextNode('New list item');
-  li.appendChild(text);
-  ul.appendChild(li);
+function deleteSymptom(symptom){
+  fetch('/delete-symptom', {
+      method: 'POST',
+      body: JSON.stringify({ symptom:symptom}),
+  }).then((_res) => {
+      window.location.href ="/";
+  });
 }
 
